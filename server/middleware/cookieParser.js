90c1c970aa8ae32d667a8a29fe839963874d04c5
Gabriel Song <gabsong4@gmail.com>
2020-04-08 01:07:52 -0700

    Implement cookieParser middleware

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,19 @@
+/**
+ * write a middleware function that will access the cookies on
+ * an incoming request, parse them into an object, and assign
+ * this object to a cookies property on the request.
+ */
 const parseCookies = (req, res, next) => {
+  const { cookie } = req.headers;
+  if (cookie) {
+    const collection = cookie.split('; ');
+    collection.forEach((str) => {
+      const [ key, value ] = str.split('=');
+      req.cookies[key] = value;
+    });
+  }
+
+  next();
 };
 
 module.exports = parseCookies;
