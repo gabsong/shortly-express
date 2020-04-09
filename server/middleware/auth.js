737cc5e55fa20887b8cfe4448e2fc3b5a1eeaaec
Gabriel Song <gabsong4@gmail.com>
2020-04-08 23:05:17 -0700

    Refactor auth createSession, add skeleton for verifySession

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,50 +3,47 @@ const Promise = require('bluebird');
 const _ = require('lodash');
 
 module.exports.createSession = (req, res, next) => {
-  req.session = {};
-  const { cookies } = req;
+  Promise.resolve(req.cookies)
+    .then((cookies) => {
+      if (_.isEmpty(cookies)) {
+        // Case 1: Has NO cookies (i.e. fresh user login)
+        throw cookies;
+      }
+      // Case 2: Has cookies
+      const { shortlyid } = cookies; // shortlyid === cookie key in tests
+      return models.Sessions.get({ hash: shortlyid });
+    })
+    .then((session) => {
+      if (!session) {
+        // Case 3: Has NO session
+        throw session;
+      }
 
-  if (!_.isEmpty(cookies)) {
-    const { shortlyid } = cookies;
-
-    // verify that the cookie is valid (stored in db)
-    models.Sessions.get({ hash: shortlyid })
-      .then((session) => {
-        if (session && session.hash === shortlyid) {
-          // cookie is valid, store existing hash in session
-          req.session.hash = session.hash;
-        } else {
-          // session does not exist or cookie is NOT valid
-          res.clearCookie('shortlyid');
-
-          // res.redirect('/login'); // does not work
-          // Error [ERR_HTTP_HEADERS_SENT]:
-          // Cannot set headers after they are sent to the client
-        }
-        return;
-      })
-      .then(() => next())
-      .catch((error) => console.log(error));
-  } else {
-    // generate a session w/ unique hash -> store in DB
-    // console.log('QQQQQQQQQ', req);
-    // console.log('SSSSSSSSS', res);
-    models.Sessions.create()
-      .then((data) => data.insertId)
-      .then((id) => models.Sessions.get({ id }))
-      .then((session) => {
-        req.session.hash = session.hash;
-        res.cookie('shortlyid', session.hash);
-      })
-      .then(() => next())
-      .catch((error) => console.log(error));
-  }
-  // look up username and userId related to this session
-  // update the session object
-  // update the session in the database record
+      // Case 4: Has session (i.e. returning user login)
+      // PLACEHOLDER: verify session is still valid
+      return session;
+    })
+    .catch(() => {
+      // create and return a new session
+      return models.Sessions.create()
+        .then((result) => models.Sessions.get({ id: result.insertId }))
+        .then((session) => {
+          res.cookie('shortlyid', session.hash);
+          return session;
+        })
+        .catch((error) => console.log(error));
+    })
+    .then((session) => {
+      // Case 5: We have a session, existing or NEW
+      req.session = session;
+      next();
+    });
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
 
+module.exports.verifySession = (req, res, next) => {
+
+};
